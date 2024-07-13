f54f6ed6cce870fc9ec14c3b86998051a4f7ef96
Stavros Charitos <polikasxaritos@gmail.com>
2024-07-13 21:08:08 +0100

    disable swagger for now

--- Program.cs
+++ Program.cs
@@ -25,11 +25,11 @@ ConfigureServices(builder.Services);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 // Enable HTTPS redirection
 app.UseHttpsRedirection();
